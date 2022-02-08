e1e42d0cab681c66a79b686d26e15de7c223c766
Ivan Romero <raguirre.ivans@gmail.com>
2022-02-08 11:53:21 -0300

    agregar comentarios

--- calc.py
+++ calc.py
@@ -1,4 +1,9 @@
+"""
+mi primer programa para realizar operaciones con numeros
+pedir al usuario que cargue un numero
+"""
 print(f'igrese un numero: ')
+# int convierte a integer lo ingresado por teclado
 a = int(input())
 print(f'igrese otro numero:')
 b = int(input())
